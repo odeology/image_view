03f211f6726e56b3619d3df9a4a84f5f3ee3f232
Adam Ormiston <adam@bitforge.us>
2012-08-03 15:15:29 -0600

    update

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,2 @@
-javascript:(function(){image_viewer=document.createElement('SCRIPT');image_viewer.type='text/javascript';image_viewer.src='https://raw.github.com/mattfff/drudge.js/master/drudge.js?' + Math.round(Math.random()*1000);document.getElementsByTagName('head')[0].appendChild(drudge_js);})();
+javascript:(function(){image_viewer=document.createElement('SCRIPT');image_viewer.type='text/javascript';image_viewer.src='https://raw.github.com/odeology/image_view/master/scrape.js?' + Math.round(Math.random()*1000);document.getElementsByTagName('head')[0].appendChild(image_viewer);})();
+
