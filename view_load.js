6f7a07de30a35a5b5ba73444426eac3f2513154b
Adam Ormiston <adam@bitforge.us>
2012-08-07 14:26:38 -0600

    changed to use github paths

--- view_load.js
+++ view_load.js
@@ -3,13 +3,13 @@
 	var image_viewer = document.createElement('script');
 
 	image_viewer.type = 'text/javascript';
-	image_viewer.src = 'http://www.bitforge.us/view_new.js?' + Math.round(Math.random()*10000);
+	image_viewer.src = 'https://raw.github.com/odeology/image_view/master/view.js?' + Math.round(Math.random()*10000);
 	document.getElementsByTagName('head')[0].appendChild(image_viewer);
 	
 	var styles = document.createElement("link");
 	styles.setAttribute("rel", "stylesheet");
 	styles.setAttribute("type", "text/css");
-	styles.setAttribute("href", 'http://bitforge.us/view.css?' + Math.round(Math.random()*10000));
+	styles.setAttribute("href", 'https://raw.github.com/odeology/image_view/master/view.css?' + Math.round(Math.random()*10000));
 
   document.getElementsByTagName("head")[0].appendChild(styles);
 }).call(this);
