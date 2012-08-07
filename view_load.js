b0ae50f8937bf9a49608cde6d6f6a4cc29681e08
Adam Ormiston <adam@bitforge.us>
2012-08-07 15:07:08 -0600

    back to default

--- view_load.js
+++ view_load.js
@@ -3,13 +3,15 @@
 	var image_viewer = document.createElement('script');
 
 	image_viewer.type = 'text/javascript';
-	image_viewer.src = 'https://raw.github.com/odeology/image_view/master/view.js?' + Math.round(Math.random()*10000);
+	//image_viewer.src = 'https://raw.github.com/odeology/image_view/master/view.js?' + Math.round(Math.random()*10000);
+	image_viewer.src = 'http://bitforge.us/test/view.js?' + Math.round(Math.random()*10000);
 	document.getElementsByTagName('head')[0].appendChild(image_viewer);
 	
 	var styles = document.createElement("link");
 	styles.setAttribute("rel", "stylesheet");
 	styles.setAttribute("type", "text/css");
-	styles.setAttribute("href", 'https://raw.github.com/odeology/image_view/master/view.css?' + Math.round(Math.random()*10000));
+	//styles.setAttribute("href", 'https://raw.github.com/odeology/image_view/master/view.css?' + Math.round(Math.random()*10000));
+	styles.setAttribute("href", 'http://bitforge.us/test/view.css?' + Math.round(Math.random()*10000));
 
   document.getElementsByTagName("head")[0].appendChild(styles);
 }).call(this);
